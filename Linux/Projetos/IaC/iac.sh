0235576dfc062e5b1b689aee82713e10183bfa5e
kleber-data <kleber.cerqueira.ads@gmail.com>
2026-06-10 21:42:06 -0300

    docs: adiciona iac.sh e README.md a Projetos

---
+++ Linux/Projetos/IaC/iac.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+# usuários, grupos de usuários, diretórios e permissões serão criadas automaticamente.
+
+# 1. criação dos diretórios
+mkdir /publico /adm /ven /sec
+
+
+# 2. Criação dos grupos de Usuários
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+# 3. Criação de Usuários já nos grupos
+useradd carlos -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
+
+
+# 4 Configuração de dono e permissões dos diretórios
+
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+
+
+
+
+
+
